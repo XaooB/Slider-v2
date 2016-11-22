d9cb2096ee29d38f68da88637cc68d2d1b008099
Bartosz <prawo123@interia.pl>
2016-11-22 01:25:57 +0100

    added fade functionality

--- slider.js
+++ slider.js
@@ -1,15 +1,22 @@
 function init(container) {
 	var nextButton = container.querySelector('.right-button');
 	var previousButton = container.querySelector('.left-button');
+	var items = container.querySelectorAll('.item');
 
 	nextButton.addEventListener('click',function(e){
-		console.log(e.target);
+	var current = container.querySelector('.item.current')
+	var next = container.querySelector(".item.current").nextElementSibling || items[0]
+			current.classList.remove('current');
+			next.classList.add('current');
 	},false)
 
 	previousButton.addEventListener('click',function(e){
-		console.log(e.target);
+	var current = container.querySelector('.item.current')
+	var previous = container.querySelector(".item.current").previousElementSibling || items[items.length-1];
+			current.classList.remove('current');
+			previous.classList.add('current');
 	},false)
 }
 
-
 init(document.querySelector('.slider-wrapper'));
+//init(document.querySelector('.slideshow'));
