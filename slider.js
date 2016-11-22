d9df3840c1d9f7bb17c059f3364424d42eca7f7b
Bartosz <prawo123@interia.pl>
2016-11-22 17:52:54 +0100

    slideshow functionality added

--- slider.js
+++ slider.js
@@ -1,6 +1,7 @@
 function init(container) {
 	var nextButton = container.querySelector('.right-button');
 	var previousButton = container.querySelector('.left-button');
+	var slider = container.querySelector('.slider');
 	var items = container.querySelectorAll('.item');
 
 	nextButton.addEventListener('click',function(e){
@@ -16,7 +17,25 @@ function init(container) {
 			current.classList.remove('current');
 			previous.classList.add('current');
 	},false)
+
+	var slideshow = false;
+	var sh = null;
+	slider.addEventListener('click',function(e) {
+		if(slideshow) {
+			clearInterval(sh)
+			slideshow = false;
+		}
+		else {
+			sh = setInterval(function(){
+			var current = container.querySelector('.item.current')
+			var next = container.querySelector(".item.current").nextElementSibling || items[0]
+			current.classList.remove('current');
+			next.classList.add('current');
+			slideshow = true;
+			},3000);
+		}
+	},false)
 }
 
-init(document.querySelector('.slider-wrapper'));
-//init(document.querySelector('.slideshow'));
+init(document.querySelector('#main-slider'));
+init(document.querySelector('#another-slider'));
